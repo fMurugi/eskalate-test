8e450e9fbe5c33c7cdb0de2bf7d67389dc168d2c
fMurugi <fionamurugi99@gmail.com>
2025-08-09 12:57:35 +0300

    feat: run with uvicorn

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from database import Base, engine 
 from routers import auth,jobs
+import uvicorn
 import models
-
+import os
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Job Portal API")
@@ -13,3 +14,11 @@ app.include_router(jobs.router)
 @app.get("/")
 def home():
     return {"message": "Welcome to the Job Portal API"}
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app", 
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000)),
+        reload=True
+    )
